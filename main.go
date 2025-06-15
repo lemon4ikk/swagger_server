d6850e6f75f33f2fce7f747fc6d38e6caa85348f
Никита Сергеев <nikit.sambo@gmail.com>
2025-06-15 20:06:10 +0300

    fix: yaml format

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Type", "application/yaml")
 		http.ServeFile(w, r, "dist/task_traker.yaml")
 	})
 
