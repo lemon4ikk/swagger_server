53b28c381e710c2872819df87bb9f048dde13e03
Никита Сергеев <nikit.sambo@gmail.com>
2025-06-15 20:16:58 +0300

    fix: main.go

--- main.go
+++ main.go
@@ -3,19 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data, err := os.ReadFile("dist/task_traker.yaml")
-		if err != nil {
-			http.Error(w, "file not found", 404)
-			return
-		}
-		w.Header().Set("Content-Type", "text/plain")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Write(data)
+		http.ServeFile(w, r, "dist/task_traker.yaml")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
